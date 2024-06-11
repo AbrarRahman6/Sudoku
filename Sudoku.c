33acb8e3dea8416028231d1b16ba15b7b15c5fab
AbrarRahman6 <abrar.rahman.241@northsouth.edu>
2024-06-12 04:14:25 +0600

    Sudoku.c

--- Sudoku.c
+++ Sudoku.c
@@ -10,39 +10,51 @@
 #define S ((int)sqrt(N))
 #define K 50
 
-typedef struct {
+typedef struct 
+{
     int value;
     int isFixed;
 } Cell;
 
 Cell mat[N][N];
 
-int CheckIfSafe(int i, int j, int num) {
+int CheckIfSafe(int i, int j, int num) 
+{
     return (unUsedInRow(i, num) && unUsedInCol(j, num) && unUsedInBox(i - i % S, j - j % S, num));
 }
 
-int unUsedInRow(int i, int num) {
-    for (int j = 0; j < N; j++) {
-        if (mat[i][j].value == num) {
+int unUsedInRow(int i, int num) 
+{
+    for (int j = 0; j < N; j++) 
+    {
+        if (mat[i][j].value == num) 
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int unUsedInCol(int j, int num) {
-    for (int i = 0; i < N; i++) {
-        if (mat[i][j].value == num) {
+int unUsedInCol(int j, int num) 
+{
+    for (int i = 0; i < N; i++) 
+    {
+        if (mat[i][j].value == num) 
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int unUsedInBox(int rowStart, int colStart, int num) {
-    for (int i = 0; i < S; i++) {
-        for (int j = 0; j < S; j++) {
-            if (mat[rowStart + i][colStart + j].value == num) {
+int unUsedInBox(int rowStart, int colStart, int num) 
+{
+    for (int i = 0; i < S; i++) 
+    {
+        for (int j = 0; j < S; j++) 
+        {
+            if (mat[rowStart + i][colStart + j].value == num) 
+            {
                 return 0;
             }
         }
@@ -50,17 +62,23 @@ int unUsedInBox(int rowStart, int colStart, int num) {
     return 1;
 }
 
-void fillDiagonal() {
-    for (int i = 0; i < N; i = i + S) {
+void fillDiagonal() 
+{
+    for (int i = 0; i < N; i = i + S) 
+    {
         fillBox(i, i);
     }
 }
 
-void fillBox(int row, int col) {
+void fillBox(int row, int col) 
+{
     int num;
-    for (int i = 0; i < S; i++) {
-        for (int j = 0; j < S; j++) {
-            do {
+    for (int i = 0; i < S; i++) 
+    {
+        for (int j = 0; j < S; j++) 
+        {
+            do 
+            {
                 num = 1 + rand() % N;
             } while (!CheckIfSafe(row + i, col + j, num));
             mat[row + i][col + j].value = num;
@@ -72,26 +90,34 @@ void fillBox(int row, int col) {
     }
 }
 
-int fillRemaining(int i) {
-    if (i >= N*N) return 1;
+int fillRemaining(int i) 
+{
+    if (i >= N*N) 
+    {
+        return 1;
+    }
 
     int row = i / N;
     int col = i % N;
 
-    if (mat[row][col].value != 0) {
+    if (mat[row][col].value != 0) 
+    {
         printSudoku(mat);
         Sleep(5);
         system("cls");
         return fillRemaining(i + 1);
     }
 
-    for (int num = 1; num <= N; num++) {
-        if (CheckIfSafe(row, col, num)) {
+    for (int num = 1; num <= N; num++) 
+    {
+        if (CheckIfSafe(row, col, num)) 
+        {
             mat[row][col].value = num;
             printSudoku(mat);
             Sleep(5);
             system("cls");
-            if (fillRemaining(i + 1)) {
+            if (fillRemaining(i + 1)) 
+            {
                 return 1;
             }
             mat[row][col].value = 0;
@@ -100,11 +126,14 @@ int fillRemaining(int i) {
     return 0;
 }
 
-void removeKDigits() {
-    for (int count = 0; count < K; ) {
+void removeKDigits() 
+{
+    for (int count = 0; count < K; ) 
+    {
         int row = rand() % N;
         int col = rand() % N;
-        if (mat[row][col].value != 0 && mat[row][col].isFixed == 0) {
+        if (mat[row][col].value != 0 && mat[row][col].isFixed == 0) 
+        {
             count++;
             mat[row][col].value = 0;
             printSudoku(mat);
@@ -118,10 +147,14 @@ void printSudoku()
 {
     for (int i = 0; i < N; i++)
     {
-        if (i==0)
+        if (i==0) 
+        {
             printf("+-------+-------+-------+\n");
+        }
         else if (i % 3 == 0 && i != 0)
+        {
             printf("+-------+-------+-------+\n");
+        }
         for (int j = 0; j < N; j++)
         {
             if (j==0)
@@ -135,28 +168,55 @@ void printSudoku()
     printf("+-------+-------+-------+\n");
 }
 
-int solve_sudoku(Cell mat[N][N], int row, int col) {
-    if (row < N && col < N) {
-        if (mat[row][col].value != 0) {
-            if ((col + 1) < N) return solve_sudoku(mat, row, col + 1);
-            else if ((row + 1) < N) return solve_sudoku(mat, row + 1, 0);
-            else return 1;
-        } else {
-            for (int num = 1; num <= N; num++) {
-                if ((CheckIfSafe(row, col, num))) {
+int solve_sudoku(Cell mat[N][N], int row, int col) 
+{
+    if (row < N && col < N) 
+    {
+        if (mat[row][col].value != 0) 
+        {
+            if ((col + 1) < N)
+            {
+                return solve_sudoku(mat, row, col + 1);
+            }
+            else if 
+            {
+                ((row + 1) < N) return solve_sudoku(mat, row + 1, 0);
+            }
+            else 
+            {
+                return 1;
+            }
+        } 
+        else 
+        {
+            for (int num = 1; num <= N; num++) 
+            {
+                if ((CheckIfSafe(row, col, num))) 
+                {
                     mat[row][col].value = num;
 
-                    if (solve_sudoku(mat, row, col)) return 1;
-                    else mat[row][col].value = 0;
+                    if (solve_sudoku(mat, row, col)) 
+                    {
+                        return 1;
+                    }
+                    else 
+                    {
+                        mat[row][col].value = 0;
+                    }
                 }
             }
         }
 
         return 0;
-    } else return 1;
+    } 
+    else 
+    {
+        return 1;
+    }
 }
 
-int main() {
+int main() 
+{
     char c=0, p='1';
     Cell copy[N][N];
     srand(time(NULL));
@@ -167,8 +227,10 @@ int main() {
         fillDiagonal();
         fillRemaining(0);
         removeKDigits();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) 
+        {
+            for (int j = 0; j < N; j++) 
+            {
                 copy[i][j] = mat[i][j];
             }
         }
@@ -178,7 +240,9 @@ int main() {
         printf("Press 1 to see solution.");
         c = getch();
         while(c!='1')
+        {
             c = getch();
+        }
         sol();
         solve_sudoku(mat, 0, 0);
         printf("Generated Sudoku:\n");
@@ -189,7 +253,9 @@ int main() {
         printf("If you want to continue press 1\nor if you want to terminate press 0 : \n\n");
         p = getch();
         while(!(p=='1'||p=='0'))
+        {
             p = getch();
+        }
     }
     return 0;
 }
